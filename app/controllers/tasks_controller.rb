e7bc2a651525b184f0ed905b1b76eb1d4b12410e
chinyakao <chinyariley@gmail.com>
2021-04-26 16:20:49 +0800

    dry: task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, :only =>[:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -11,31 +12,32 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save
 
-    redirect_to tasks_url
+    redirect_to tasks_url, :notice => "Create Notice: 建立成功!"
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
-    redirect_to task_path(@task)
+    redirect_to task_path(@task), :notice => "Update Notice: 更新成功!"
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_url
+    redirect_to tasks_url, :notice => "Destroy Notice: 刪除成功"
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])     #重複宣告的實例變數
+  end
+
   def task_params
     params.require(:task).permit(:title, :content)
   end
